786d460e0b9dfd68e69dd1e34acbb811f7189f0c
QuXiaoMing <xm726@qq.com>
2017-08-28 17:09:22 +0800

    #fix del some console

--- js/zoom.js
+++ js/zoom.js
@@ -2,11 +2,10 @@
 // V 0.1.0
 // 2016年7月18日
 
-var windowHight = $(window).height();
-var windowWidth = $(window).width();
-// console.log(windowHight,windowWidth);
-var BoxIsShowed = false;
-var zoomBox = $('<div id="qxm"></div>').css({
+let windowHight = $(window).height();
+let windowWidth = $(window).width();
+let BoxIsShowed = false;
+let zoomBox = $('<div"></div>').css({
   'position': 'fixed',
   'pointer-events': 'none',
   'display': 'none',
@@ -18,30 +17,29 @@ var zoomBox = $('<div id="qxm"></div>').css({
   'z-index': '999'
 });
 $('<img>').attr('src', ' ').appendTo(zoomBox);
-var resetZoomBox = function () {
-  var img = zoomBox.find('img');
-  var imgHeight = img.height();
-  var imgWidth = img.width();  
+let resetZoomBox = function () {
+  let img = zoomBox.find('img');
+  let imgHeight = img.height();
+  let imgWidth = img.width();
   if (imgHeight > windowHight) {
     img.height(windowHight - 80);
-    console.log(imgHeight, windowHight)
   }
   zoomBox.css({
     'top': 0.5 * (45 + windowHight - img.height()),
     'height': img.height()
   })
 }
-var intZoomBox = function () {
+let intZoomBox = function () {
   zoomBox.appendTo($('body'));
   console.log(zoomBox);
   console.log('div has appeded');
 }
 intZoomBox();
-function getRealSrc(src) {
+function getRealSrc (src) {
   // 缩略图地址 http://ww2.sinaimg.cn/orj480/718878b5jw1f5gqz9yy4hj20go0c8go5.jpg
   // 真实地址 http://ww2.sinaimg.cn/mw1024/718878b5jw1f5gqz9yy4hj20go0c8go5.jpg
-  var str = ''
-  var zoomBoxSHowed = false;
+  let str = ''
+  let zoomBoxSHowed = false;
   if (src.indexOf('orj480') > 0) {
     str = src.replace('orj480', 'mw1024');
   }
@@ -50,36 +48,29 @@ function getRealSrc(src) {
   }
   return str
 }
-var showZoomBox = function (realSrc) {
+let showZoomBox = function (realSrc) {
   zoomBox.find('img').attr('src', realSrc);
   zoomBox.show();
   resetZoomBox();
-  console.log('div has show');
 }
-var hideZoomBox = function () {
+let hideZoomBox = function () {
   zoomBox.hide();
-  console.log('div has hide');
 }
 setTimeout(function () {
-  //processing 
-  //   console.log('setTimeouBagin');
-  mediaBox = $('.media_box img').parent();
+  let mediaBox = $('.media_box img').parent();
   mediaBox.mouseover(function () {
     if (!BoxIsShowed) {
-      var src = $(this).find('img').attr('src');
-      var realSrc = getRealSrc(src);
-      console.log($(this).offset().top, $(this).offset().left) //       console.log(src);
-      //       console.log(realSrc);      
+      let src = $(this).find('img').attr('src');
+      let realSrc = getRealSrc(src);
       showZoomBox(realSrc);
       BoxIsShowed = true;
     }
-  }
-  ).mouseleave(function () {
+  }).mouseleave(function () {
     if (BoxIsShowed) {
       hideZoomBox();
       BoxIsShowed = false;
     }
-  }
-  ) //   console.log('setTimeouEnd');
+  })
+  // 更新 mediaBox
   setTimeout(arguments.callee, 1000);
 }, 1000);
